9f0b2989071551a26d110af2c70df834a502136d
Yun <y5624711@gmail.com>
2024-10-22 12:19:19 +0900

    게시물 수정 form

--- src/main/java/com/example/prjjsp20241022/controller/BoardController.java
+++ src/main/java/com/example/prjjsp20241022/controller/BoardController.java
@@ -4,9 +4,12 @@ import com.example.prjjsp20241022.dto.Board;
 import com.example.prjjsp20241022.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,13 +18,45 @@ public class BoardController {
 
     private final BoardService service;
 
+    // 게시물 CRUD
+
+    // /board/new
     @GetMapping("new")
-    public void newBoard(){
+    public void newBoard() {
+
         // /WEB-INF/view/board/new.jsp
     }
 
     @PostMapping("new")
-    public void newBoard(Board board){
+    public String newBoard(Board board) {
         service.add(board);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("list")
+    public void listBoard(Model model) {
+        List<Board> list = service.list();
+        model.addAttribute("boardList", list);
+
+    }
+
+    @GetMapping("view")
+    public void viewBoard(Integer id, Model model) {
+        Board board = service.get(id);
+        model.addAttribute("board", board);
+    }
+
+    @PostMapping("delete")
+    public String deleteBoard(Integer id) {
+        service.remove(id);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("edit")
+    public void editBoard(Integer id, Model model) {
+        Board board = service.get(id);
+        model.addAttribute("board", board);
     }
 }
---
+++ src/main/webapp/WEB-INF/view/board/view.jsp
@@ -0,0 +1,39 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<c:import url="/WEB-INF/fragment/navbar.jsp"/>
+<h2>${board.id}번 게시물</h2>
+
+<div>
+    제목
+    <input type="text" value="${board.title}" readonly>
+</div>
+<div>
+    본문
+    <textarea id="" cols="30" rows="10" readonly>${board.content}</textarea>
+</div>
+<div>
+    작성자
+    <input type="text" value="${board.writer}" readonly>
+</div>
+<div>
+    작성일시
+    <input type="datetime-local" value="${board.inserted}" readonly>
+</div>
+
+<div>
+    <form action="/board/delete" method="post">
+        <input type="hidden" name="id" value="${board.id}">
+        <button>삭제</button>
+    </form>
+</div>
+<div>
+    <a href="/borad/edit?id=${board.id}">수정</a>
+</div>
+
+</body>
+</html>
