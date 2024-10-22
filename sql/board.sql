a875562d629788eb7adbda0b5a7737edc8c28d02
Yun <y5624711@gmail.com>
2024-10-22 10:27:40 +0900

    jsp

--- sql/board.sql
+++ sql/board.sql
@@ -1,11 +1,13 @@
 use jsp;
 
-CREATE TABLE board(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(10) NOT NULL ,
-    content VARCHAR(5000) NOT NULL ,
-    writer VARCHAR(200) NOT NULL ,
-    inserted DATETIME NOT NULL DEFAULT NOW()
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(10)   NOT NULL,
+    content  VARCHAR(5000) NOT NULL,
+    writer   VARCHAR(200)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
 );
 
-SELECT * FROM board;
+SELECT *
+FROM board;
