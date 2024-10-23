568cf7603b87f1f28cbd0ca5e04d2a8ef6d7ad09
Yun <y5624711@gmail.com>
2024-10-23 09:50:27 +0900

    페이지 정보 담기 & 오른쪽 끝 페이지는 마지막 페이지 보다 클 수없음

--- src/main/java/com/example/prjjsp20241022/service/BoardService.java
+++ src/main/java/com/example/prjjsp20241022/service/BoardService.java
@@ -21,22 +21,33 @@ public class BoardService {
     }
 
     public Map<String, Object> list(Integer page) {
-        Integer offset = (page - 1) * 10;//한 페이지에 10개
+        // 한 페이지에 10개
+        Integer offset = (page - 1) * 10;
 
 //        List<Board> list = mapper.selectAll();
         List<Board> list = mapper.selectAllPaging(offset);
 
-        //컨트롤러에게 넘겨줄 정보들을 담을 배열
+        // Controller에게 넘겨 줄 정보들을 담을 map
         Map<String, Object> map = new HashMap<>();
 
-//        페이지 관련 정보들
-        Integer countAll = mapper.countAll();//총 레코드 수
-        Integer lsatPageNumber = (countAll - 1) / 10 + 1;//마지막 페이지 번호
+        // 페이지 관련 정보들
+        Integer countAll = mapper.countAll();
+        Integer lastPageNumber = (countAll - 1) / 10 + 1; // 마지막 페이지 번호
+        Integer rightPageNumber = ((page - 1) / 10 + 1) * 10; // 현재페이지 기준 오른쪽 끝 페이지 번호
+        Integer leftPageNumber = rightPageNumber - 9;// 현재페이지 기준 왼쪽 끝 페이지 번호
 
+        // 오른쪽 끝페이지는 마지막 페이지 보다 클 수 없음
+        rightPageNumber = Math.min(rightPageNumber, lastPageNumber);
 
-        //컨트롤러에게 넘겨주기위해 값을 담음
-        map.put("lastPageNumber", lsatPageNumber);
+        Map<String, Object> pageInfo = new HashMap<>();
+
+        pageInfo.put("leftPageNumber", leftPageNumber);
+        pageInfo.put("rightPageNumber", rightPageNumber);
+        pageInfo.put("lastPageNumber", lastPageNumber);
+
+        map.put("pageInfo", pageInfo);
         map.put("boardList", list);
+
 
         return map;
     }
