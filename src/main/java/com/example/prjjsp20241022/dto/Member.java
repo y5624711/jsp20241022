2250d95469a425d06562fc571b511a0010acc2a2
Yun <y5624711@gmail.com>
2024-10-24 17:37:30 +0900

    회원가입연습

--- src/main/java/com/example/prjjsp20241022/dto/Member.java
+++ src/main/java/com/example/prjjsp20241022/dto/Member.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 public class Member {
     private String id;
     private String password;
+    private String nickName;
     private String description;
     private LocalDateTime inserted;
 }
